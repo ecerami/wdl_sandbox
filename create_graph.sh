627d1c8580f53e3980653987e010724650d49caf
Ethan Cerami <cerami@jimmy.harvard.edu>
2019-03-12 22:14:31 -0400

    Added create graph and google json

---
+++ create_graph.sh
@@ -0,0 +1,1 @@
+java -jar womtool-38.jar graph ecoli.wdl
--- run_ecoli_cloud.sh
+++ run_ecoli_cloud.sh
@@ -1,1 +1,3 @@
-java -Dconfig.file=google.conf -jar /home/cerami/cromwell/cromwell-38.jar run ecoli.wdl -i ecoli.json
+python create_inputs.py ecoli_base.json google > ecoli_google.json
+java -jar womtool-38.jar validate ecoli.wdl
+java -Dconfig.file=google.conf -jar cromwell-38.jar run ecoli.wdl -i ecoli_google.json
